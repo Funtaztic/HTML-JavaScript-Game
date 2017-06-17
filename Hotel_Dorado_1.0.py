a1e54b94dfbaad472c6db46dde3196a938877520
Funtaztic <abelszabolcsi@gmail.com>
2017-06-17 19:13:59 +0200

    Liars included :D
    Go with your GoT feeling xD

--- Hotel_Dorado_1.0.py
+++ Hotel_Dorado_1.0.py
@@ -1,3 +1,10 @@
 # This will be a TRPG, Text-Based adventure game with
 # lots of rooms, friend, enemies, hidden treasures, secrets,
 # collectibles and more. Gain XP, save HP, have FUN! =D
+# (listening to GameOfThrones music)...make them LIE! xD
+# The player can not trust anyone! (after enough interactions)
+
+# Variables:
+HP = 100
+XP = 0
+MONEY = 0
